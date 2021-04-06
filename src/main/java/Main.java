2fb3f7459fc0dd0eac780578a910dadadc9e79de
Yulia Kobeleva <kobelevaulia@gmail.com>
2021-04-06 23:10:45 +0300

    https://github.com/TakkiDakki/Number-5.2/issues/1#issue-851767076

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,16 @@ public class Main {
   public static void main(String[] args) {
     BonusService service = new BonusService();
 
+
     long amount = 1000_60;
     boolean registered = true;
+    long expected = 60;
 
-    service.calculate(amount, registered);
-    }
+    long actual = service.calculate(amount, registered);
+
+    boolean passed = expected == actual;
+    System.out.println(passed);
+
+  }
 }
 
